8a83a308fcf91361cc8e84e231e70e51a1ec7415
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-12-01 20:44:58 +0900

    여러 post들을 불러오는 get 요청

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -5,6 +5,7 @@ import { PostsService } from './posts.service';
 // nest g resource / posts
 // 터미널에 명령어를 입력하면 posts로 시작되는 엔드포인트들을 관리하는
 // 와우 개편함; 이게 바로 nest cli
+// 관련 있는 모듈까리 묶어가지고 코드를 관리하게 된다..
 
 describe('PostsController', () => {
   let controller: PostsController;
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,27 +4,72 @@ import { PostsService } from './posts.service';
 // 새로운 터미널, nest g resource / posts 로 생성했더니 와 익 ㅔ뭐야
 // 자동으로 만들어지네
 
-interface Post{
-  author : String;
-  title : String;
+interface PostModel {
+  id : number;
+  author : string;
+  title : string;
   content : string;
   likeCount : number;
   commentCount : number;
 }
 
+let posts : PostModel[] = [
+
+  {
+    id : 1,
+    author : "newjeans_officeal",
+    title : "개쩌는 대단한 엄청나게 대단한 락밴드",
+    content: "린킨파크",
+    likeCount : 1000000,
+    commentCount : 9999999
+  } ,
+
+  {
+    id : 2,
+    author : "newjeans_officeal",
+    title : "개쩌는 대단한 엄청나게 대단한 락밴드2",
+    content: "펄잼",
+    likeCount : 1000000,
+    commentCount : 9999999
+  } ,
+
+  {
+    id : 3,
+    author : "blackPink_officeal",
+    title : "로제",
+    content: "아파트아파틍ㅍㅌㅍㅇㅌ",
+    likeCount : 1000000,
+    commentCount : 9999999
+  }
+]
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET/posts
+  // 모든 post를 다 가져온다.
+
   @Get()
-  getPost(): Post {
-    return {
-      author : "newjeans_officeal",
-      title : "뉴진스 민지",
-      content: "공룡 민지",
-      likeCount : 1000000,
-      commentCount : 9999999
-    };
+  getPosts() {
+    return posts;
   }
+  // 2) GET/posts/:id
+  // id에 해당되는 post를 가져온다
+  // 예를들어 id=1인 경우 ids가 1인 포스트를 가져온다
+
+ // 3) POST /post
+ // 포스트를 생성한다
+ 
+ // 4) PUT /posts/:id
+  // id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  // id에 있는 POST를 삭제한다.
+
+  
+
+
+
 
 }
